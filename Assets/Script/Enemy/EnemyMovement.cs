66559c73323d0afe7e4878e0c19014df41331249
Enes Erden <mehmeteneserden@gmail.com>
2025-07-04 16:32:19 +0300

    Düşman hareket koduna archer için mesafe şartı eklendi

--- Assets/Script/Enemy/EnemyMovement.cs
+++ Assets/Script/Enemy/EnemyMovement.cs
@@ -7,6 +7,7 @@ public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform player;
+    public EnemyData enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,32 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Yön vektörü ve mesafe hesapla
+        Vector3 direction = player.position - transform.position;
+        float distance = direction.magnitude;
+
         if (player != null)
         {
-            agent.SetDestination(player.position);
+            if (enemy.enemyName == "Archer")
+            {
+                Debug.Log($"ARCHER MESAFE: {distance}, RANGE: {enemy.range}");
+                if (distance > enemy.range)
+                {
+                    Debug.Log("MESAFE ÇOK UZAK");
+                    // Okcunun range i kadar uzakta kalacak oradan saldıracak.
+                    Vector3 targetPosition = player.position - direction.normalized * enemy.range;
+                    agent.SetDestination(targetPosition);
+                }
+                else
+                {
+                    // Yeterince yakınsa dur
+                    agent.ResetPath();
+                }
+            }
+            else
+            {
+                agent.SetDestination(player.position);
+            }
         }
     }
 }
